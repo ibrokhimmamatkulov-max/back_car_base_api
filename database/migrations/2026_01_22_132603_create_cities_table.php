49a5af38c2a99ab15dd444e6b04d3d2e0391fc5a
nazarov24 <sardor.abdunazarov@invox.ru>
2026-05-05 14:13:27 +0500

    fix

--- database/migrations/2026_01_22_132603_create_cities_table.php
+++ database/migrations/2026_01_22_132603_create_cities_table.php
@@ -11,12 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('cities', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('description')->nullable();
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('cities')) {
+            Schema::create('cities', function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->string('description')->nullable();
+                $table->timestamps();
+            });
+        }
     }
 
     /**
